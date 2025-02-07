5ed2018e696e937950c0aa0e6ead902ceda9a70c
Kolesnyk Mykhailo <devhomeworks@gmail.com>
2025-02-07 17:43:25 +0200

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,168 +1,201 @@
-const   {task, watch, src, dest, parallel, series} = require('gulp'),
-        browserSync = require('browser-sync'),
-        sass = require('gulp-sass')(require('sass')),
-        uglify = require('gulp-uglify'),
-        babel = require('gulp-babel'),
-        fileinclude = require('gulp-file-include'),
-        prefixer = require('gulp-autoprefixer'),
-        imagemin = require('gulp-imagemin'),
-        sourcemaps = require('gulp-sourcemaps'),
-        cssnano = require('gulp-cssnano'),
-        rimraf = require('rimraf'),
-        rename = require('gulp-rename'),
-        stream = browserSync.stream
-        
+const { task, watch, src, dest, parallel, series } = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-dart-sass');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const fileinclude = require('gulp-file-include');
+const prefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('gulp-cssnano');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
+const stream = browserSync.stream;
 
 //----------------------------------- Path ----------------------------//
 
-const  path = {
-    build: {
-        html: 'build',
-        css: 'build/css',
-        img: 'build/img',
-        js: 'build/js',
-        fonts: 'build/fonts',
-    },
-    src: {
-        html: 'src/html/*.html',
-        scss: 'src/styles/*.sass',
-        img: 'src/img/**',
-        js: 'src/js/*.js',
-        jsLib: 'src/js-lib/*.js',
-        fonts: 'src/fonts/**'
-    },
-    watch: {
-        html: 'src/html/**',
-        scss: 'src/styles/**',
-        js: 'src/js/**',
-        img: 'src/img/**',
-        fonts: 'src/fonts/**'
-    }
+const path = {
+	build: {
+		html: 'build',
+		css: 'build/css',
+		img: 'build/img',
+		video: 'build/video',
+		js: 'build/js',
+		fonts: 'build/fonts',
+	},
+	src: {
+		html: 'src/html/*.html',
+		scss: 'src/styles/*.scss',
+		cssLib: 'src/css-lib/*.css',
+		img: 'src/img/**',
+		video: 'src/video/**',
+		js: 'src/js/*.js',
+		jsLib: 'src/js-lib/*.js',
+		fonts: 'src/fonts/**',
+	},
+	watch: {
+		html: 'src/html/**',
+		scss: 'src/styles/**',
+		cssLib: 'src/css-lib/**',
+		js: 'src/js/**',
+		jsLib: 'src/js-lib/**',
+		img: 'src/img/**',
+		video: 'src/video/**',
+		fonts: 'src/fonts/**',
+	},
 };
 
 //----------------------------------- Html ----------------------------//
 
-const html = function() {
-    return src(path.src.html)
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(dest(path.build.html))
-        .pipe(stream())
+const html = function () {
+	return src(path.src.html)
+		.pipe(
+			fileinclude({
+				prefix: '@@',
+				basepath: '@file',
+			})
+		)
+		.pipe(dest(path.build.html))
+		.pipe(stream());
 };
 
 //----------------------------------- Style ----------------------------//
 
 const styles = function () {
-    return src(path.src.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        // .pipe(prefixer()) 
-        // .pipe(cssnano())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(path.build.css))
-        .pipe(stream());
+	return (
+		// src(['src/styles/*.sass', 'src/styles/*.scss'])
+		// src(['src/styles/**/*.sass', 'src/styles/**/*.scss'])
+		src(['src/styles/style.scss'])
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(prefixer())
+			// .pipe(cssnano())
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(sourcemaps.write('.'))
+			.pipe(dest(path.build.css))
+			.pipe(stream())
+	);
 };
 
-const styles_without_min = function (){
-    return src(path.src.scss)
-    .pipe(sass())
-    .pipe(prefixer())
-    .pipe(dest(path.build.css))
-}
+const styles_without_min = function () {
+	return src(path.src.scss)
+		.pipe(sass())
+		.pipe(prefixer())
+		.pipe(dest(path.build.css));
+};
+
+//----------------------------------- css_libs ----------------------------//
+const css_libs = function () {
+	return src(path.src.cssLib).pipe(dest(path.build.css)).pipe(stream());
+};
 
 //----------------------------------- Fonts ----------------------------//
 
-const fonts = function() {
-    return src(path.src.fonts)
-        .pipe(dest(path.build.fonts))
-        .pipe(stream());
+const fonts = function () {
+	return src(path.src.fonts).pipe(dest(path.build.fonts)).pipe(stream());
 };
 
 //----------------------------------- JS ----------------------------//
 
-const lib_scripts = function() {
-    console.log('sadsd');
-    return src(path.src.jsLib) 
-        .pipe(dest(path.build.js))
-}
+const lib_scripts = function () {
+	return src(path.src.jsLib).pipe(dest(path.build.js));
+};
 
-const main_scripts = function() {
-    return src(path.src.js) 
-        .pipe(babel({
-            presets: [
-                [
-                    "@babel/preset-env",
-                    {
-                        exclude: ["transform-regenerator"]
-                    },
-                ]
-            ]
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(uglify()) 
-        .pipe(rename({suffix: '.min'})) 
-        .pipe(sourcemaps.write('./'))
-        .pipe(dest(path.build.js))
-        .pipe(stream());
-}
+const main_scripts = function () {
+	return src(path.src.js)
+		.pipe(
+			babel({
+				presets: [
+					[
+						'@babel/preset-env',
+						{
+							exclude: ['transform-regenerator'],
+						},
+					],
+				],
+			})
+		)
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write('./'))
+		.pipe(dest(path.build.js))
+		.pipe(stream());
+};
 
-const scripts = parallel( main_scripts )
+const scripts = parallel(main_scripts);
 
 const scripts_without_min = function () {
-    return src(path.src.js) 
-        .pipe(babel({
-            presets: [
-                [
-                    "@babel/preset-env",
-                    {
-                        exclude: ["transform-regenerator"]
-                    },
-                ]
-            ]
-        }))
-        .pipe(dest(path.build.js))
+	return src(path.src.js)
+		.pipe(
+			babel({
+				presets: [
+					[
+						'@babel/preset-env',
+						{
+							exclude: ['transform-regenerator'],
+						},
+					],
+				],
+			})
+		)
+		.pipe(dest(path.build.js));
 };
 
 //----------------------------------- Image ----------------------------//
 
 const image = function () {
-    return src(path.src.img) 
-        .pipe(imagemin())
-        .pipe(dest(path.build.img))
-        .pipe(stream());
+	return src(path.src.img)
+		.pipe(imagemin())
+		.pipe(dest(path.build.img))
+		.pipe(stream());
 };
+//----------------------------------- Video ----------------------------//
 
+const video = function () {
+	return src(path.src.video).pipe(dest(path.build.video)).pipe(stream());
+};
 //----------------------------------- Serve ----------------------------//
 
-const build = parallel(html, lib_scripts, styles, fonts, scripts, image)
+const build = parallel(
+	html,
+	lib_scripts,
+	styles,
+	fonts,
+	scripts,
+	image,
+	css_libs,
+	video
+);
 
-const without_min = parallel(styles_without_min, scripts_without_min, lib_scripts)
+const without_min = parallel(
+	styles_without_min,
+	scripts_without_min,
+	lib_scripts
+);
 
-const webserver = function() {
-    return browserSync.init({
-        server: {
-            baseDir: "./build"
-        }
-    });
+const webserver = function () {
+	return browserSync.init({
+		server: {
+			baseDir: './build',
+		},
+	});
 };
 
-const serve = () => {
-    build()
-    webserver()
-    watch(path.watch.html, html);
-    watch(path.watch.scss, styles);
-    watch(path.src.fonts, fonts);
-    watch(path.src.js, scripts);
-    watch(path.src.jsLib, lib_scripts);
-    watch(path.src.img, image);
-}
+const serve = series(build, function () {
+	webserver();
+	watch(path.watch.html, html);
+	watch(path.watch.scss, styles);
+	watch(path.watch.fonts, fonts);
+	watch(path.watch.js, scripts);
+	watch(path.watch.jsLib, lib_scripts);
+	watch(path.watch.img, image);
+	watch(path.watch.cssLib, css_libs);
+	watch(path.watch.video, video);
+});
 
 //----------------------------------- Exports ----------------------------//
 
+exports.build = series(build, without_min);
 
-exports.build = series(build, without_min)
-
-exports.default = serve
+exports.default = serve;
--- src/js/app.js
+++ src/js/app.js
@@ -1,341 +1,120 @@
 function App() {
-  function offset(el) {
-    var rect = el?.getBoundingClientRect(),
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect?.top + scrollTop };
-  }
+	//// video
+	const video = document.getElementById('myVideo');
+	const playButton = document.getElementById('playButton');
+	const closeButton = document.getElementById('closeButton');
+	const videoContainer = document.querySelector('.video-container');
+	// Перевірка стану відео при завантаженні сторінки
+	updateVideoState();
+	playButton.addEventListener('click', () => {
+		if (video.paused) {
+			video.play();
+			playButton.innerHTML = '❚❚'; // Міняємо на паузу
+			videoContainer.classList.remove('paused');
+			videoContainer.classList.add('playing');
+		} else {
+			video.pause();
+			playButton.innerHTML = '▶'; // Міняємо на Play
+			videoContainer.classList.remove('playing');
+			videoContainer.classList.add('paused');
+		}
+	});
+	// Якщо відео закінчилося — повертаємо кнопку "Play"
+	video.addEventListener('ended', () => {
+		playButton.innerHTML = '▶';
+		videoContainer.classList.remove('playing');
+		videoContainer.classList.add('paused');
+	});
+	// Закриття відео
+	closeButton.addEventListener('click', () => {
+		video.pause(); // Зупинити відео
+		video.currentTime = 0; // Перемотати на початок
+		playButton.innerHTML = '▶'; // Повернути кнопку Play
+		videoContainer.classList.remove('playing');
+		videoContainer.classList.add('paused');
+		// Повертаємо постер, коли відео зупинене
+		video.load(); // Відновлюємо постер
+	});
+	// Оновлюємо стан відео на початку
+	function updateVideoState() {
+		if (video.paused) {
+			videoContainer.classList.add('paused');
+			videoContainer.classList.remove('playing');
+			playButton.innerHTML = '▶';
+		} else {
+			videoContainer.classList.add('playing');
+			videoContainer.classList.remove('paused');
+			playButton.innerHTML = '❚❚';
+		}
+	}
 
-  let tabs = document.querySelectorAll(".js-tab");
-  let tabContents = document.querySelectorAll(".js-tab-content");
+	let sliderPortfolio = new Swiper('.portfolio__slider', {
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		spaceBetween: 10,
+		slidesPerView: 1,
+		mousewheel: {
+			invert: false,
+		},
+		breakpoints: {
+			900: {
+				spaceBetween: 20,
+				slidesPerView: 3,
+			},
+			768: {
+				spaceBetween: 10,
+				slidesPerView: 2,
+			},
+			600: {
+				spaceBetween: 10,
+				slidesPerView: 2,
+			},
+		},
+	});
 
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", (event) => {
-      let tabsChildren = event.target.parentElement.children;
-      for (let t = 0; t < tabsChildren.length; t++) {
-        tabsChildren[t].classList.remove("tab__active");
-      }
+	// scroll header
+	const headerFixed = document.querySelector('.header');
 
-      tabs[i].classList.add("tab__active");
+	const setFixedHeader = () => {
+		headerFixed.classList.toggle('scroll-active', window.scrollY >= 60);
+	};
 
-      let tabContentChildrens =
-        event.target.parentElement.nextElementSibling.children;
+	window.addEventListener('scroll', setFixedHeader);
 
-      for (let c = 0; c < tabContentChildrens.length; c++) {
-        tabContentChildrens[c].classList.remove("tab__content-active");
-      }
+	/// header burger
 
-      tabContents[i].classList.add("tab__content-active");
-    });
-  }
+	const burger = document.querySelector('.burger');
+	const header = document.querySelector('.header');
+	const navItems = document.querySelectorAll('.nav__item');
+	const body = document.body;
 
-  //// slider
+	function closeMenu() {
+		burger.classList.remove('active');
+		header.classList.remove('open-menu');
+		body.style.overflow = 'visible';
+	}
 
-  
-  let sliderCompany = new Swiper(".js-company-slider", {
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    speed: 5200,
-    simulateTouch: false,
-    autoplay: {
-      delay: 0,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      1800: {
-        spaceBetween: 80,
-      },
-      900: {
-        spaceBetween: 50
-      }
-    },
-  });
-  
-  let sliderBeyond = new Swiper(".beyond-slider", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    spaceBetween: 10,
-    loop: true,
-    slidesPerView: 1,
-    breakpoints: {
-      1800: {
-        spaceBetween: 40,
-        slidesPerView: 3,
-      },
-      1200: {
-        spaceBetween: 20,
-        slidesPerView: 3,
-      },
-      768: {
-        spaceBetween: 10,
-        slidesPerView: 2,
-      },
-    },
-  });
-  //// faq accordion
-  let acc = document.getElementsByClassName("accordion-item");
-  let i;
+	function toggleMenu() {
+		const isActive = burger.classList.toggle('active');
+		header.classList.toggle('open-menu', isActive);
+		body.style.overflow = isActive ? 'hidden' : 'visible';
+	}
+	burger.addEventListener('click', toggleMenu);
 
-  for (i = 0; i < acc.length; i++) {
-    let head = acc[i].querySelector(".accordion-item__head");
-    head.addEventListener("click", function () {
-      let parentAcc = this.closest(".accordion-item");
-      parentAcc.classList.toggle("active");
-
-      let panel = parentAcc.querySelector(".accordion-item__body");
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
-
-  // scroll header
-  const setFixedHeader = () => {
-    if (window.scrollY >= 60 || window.pageYOffset >= 60) {
-      document
-        .getElementsByClassName("header")[0]
-        .classList.add("scroll-active");
-    } else {
-      document
-        .getElementsByClassName("header")[0]
-        .classList.remove("scroll-active");
-    }
-  }
-
-  //// parallax
-
-  window.addEventListener("scroll", () => {
-    let { scrollY } = window;
-
-    let parallaxElem = document.getElementsByClassName("parallax");
-    for (let i = 0; i < parallaxElem.length; i++) {
-      let item = parallaxElem[i];
-
-      if (item.scrollTop < scrollY) {
-        item.style.top = 0.5 * scrollY - item.scrollTop + "px";
-      } else {
-        item.style.top = '0px'
-      }
-    }
-  });
-
-  /// header burger
-
-  const burger = document.querySelector(".burger");
-  const header = document.querySelector(".header");
-  const body = document.querySelector("body");
-
-  function toggleMenu() {
-    let burgerClasses = burger.classList.value;
-    if (burgerClasses.includes("active")) {
-      burger.classList.remove("active");
-      header.classList.remove("open-menu");
-      body.style.overflow = "visible";
-      header.querySelector(".js-dropdown.active").classList.remove("active");
-    } else {
-      burger.classList.add("active");
-      header.classList.add("open-menu");
-      body.style.overflow = "hidden";
-    }
-  }
-
-  burger.addEventListener("click", toggleMenu);
-
-  /// append login
-  const login = document.querySelector(".login");
-  const nav = document.querySelector(".nav");
-  const headerInner = document.querySelector(".header__inner");
-
-  function appendLogin() {
-    if (window.innerWidth < 900) {
-      nav?.append(login);
-    } else {
-      headerInner?.append(login);
-      burger.classList.remove("active");
-      header.classList.remove("open-menu");
-      body.style.overflow = "visible";
-    }
-  }
-
-  // mobile menu
-
-  const dropdownMenu = document.querySelector(".header");
-
-  dropdownMenu.addEventListener("click", (event) => {
-    if (event.target.className.includes("js-dropdown")) {
-      event.target.classList.add("active");
-    }
-
-    if (event.target.className.includes("js-back")) {
-      let parentElem = event.target.closest(".js-dropdown");
-      parentElem.classList.remove("active");
-    }
-  });
-
-  //// scroll down
-
-  document.body.addEventListener("click", (event) => {
-    const item = event?.target;
-    if (item.className.includes("js-offset-scroll")) {
-      const offsetId = item.dataset.offset;
-      const offsetElement = document.querySelector(offsetId);
-
-      offsetElement.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-
-  // sticky tab header
-
-  let headerItem = document.getElementById("myHeader");
-
-  const parentItem = headerItem?.closest("section");
-  function setTabsSticky() {
-    let offsetParent = offset(parentItem);
-    if (
-      window.scrollY >= offsetParent.top &&
-      parentItem.offsetHeight + offsetParent.top > window.scrollY
-    ) {
-      headerItem?.classList.add("sticky");
-    } else {
-      headerItem?.classList.remove("sticky");
-    }
-  }
-
-  function setActiveTab() {
-    let activeTab = null;
-    let tabBlocks = document.querySelectorAll(".js-tab-block");
-    let tabs = document.querySelectorAll(".js-offset-scroll");
-    for (let i = 0; i < tabBlocks.length; i++) {
-      let offsetTab = offset(tabBlocks[i]);
-      if (window.scrollY >= offsetTab.top) {
-        activeTab = tabBlocks[i].id;
-      }
-    }
-
-    for (let j = 0; j < tabs.length; j++) {
-      if (`#${activeTab}` === tabs[j]?.dataset.offset) {
-        tabs[j].classList.add("tab__active");
-      } else {
-        tabs[j].classList.remove("tab__active");
-      }
-    }
-    // console.log(activeTab);
-  }
-
-  // gsap plug
-
-  gsap.registerPlugin(MotionPathPlugin);
-
-  const animates = document.getElementsByClassName("js-animate");
-
-  for (let i = 0; i < animates.length; i++) {
-    let rectSpeed = null;
-    let rect = null;
-    if (animates[i].querySelector(".plane")) {
-      rect = animates[i].querySelector(".plane");
-      rectSpeed = 25;
-    } else if (animates[i].querySelector(".ship")) {
-      rect = animates[i].querySelector(".ship");
-      rectSpeed = 50;
-    }
-
-    let path = animates[i].querySelector(".path");
-
-    gsap.set(rect, { scaleX: -1, duration: 0.5 });
-
-    if (rect && path) {
-      gsap
-        .timeline({ repeat: -1 })
-        .from(rect, {
-          motionPath: {
-            path: path,
-            align: path,
-            alignOrigin: [0.5, 0.5],
-            autoRotate: true,
-          },
-          duration: rectSpeed,
-          ease: "none",
-        })
-        .to(rect, { scaleX: 1, duration: 0.5 })
-        .to(rect, {
-          motionPath: {
-            path: path,
-            align: path,
-            alignOrigin: [0.5, 0.5],
-            autoRotate: true,
-          },
-          duration: rectSpeed,
-          ease: "none",
-        })
-        .to(rect, { scaleX: -1, duration: 0.5 });
-    }
-  }
-
-  // running numbers
-
-  const time = 3000;
-  const step = 1;
-
-  function outNum(num, elem) {
-    let n = 0;
-    let t = Math.round(time / (num / step));
-    let interval = setInterval(() => {
-      n = n + step;
-      if (n == num) {
-        clearInterval(interval);
-      }
-      elem.innerHTML = n;
-    }, t);
-  }
-
-  let executed = false;
-
-  function toVisibleElem(classes, innerClass) {
-    let elem = document.getElementsByClassName(classes);
-    let visibleArea = window.innerHeight + window.scrollY;
-
-    for (let i = 0; i < elem?.length; i++) {
-      if (visibleArea > elem[i]?.offsetTop) {
-        elem[i].classList.add("loading");
-        if (!executed) {
-          executed = true;
-
-          let runningNumbers = document.getElementsByClassName(innerClass);
-          for (let j = 0; j < runningNumbers?.length; j++) {
-            if (visibleArea > runningNumbers[j]?.offsetTop) {
-              let num = runningNumbers[j].innerHTML;
-              outNum(num, runningNumbers[j]);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  
-  window.onscroll = function () {
-    setFixedHeader();
-  };
-
-  window.addEventListener("scroll", () => {
-    toVisibleElem("js-running-value", "js-running-number");
-    setTabsSticky();
-    setActiveTab();
-  });
-
-  window.addEventListener("resize", () => {
-    appendLogin();
-  });
-
-  document.addEventListener("DOMContentLoaded", () => {
-    appendLogin();
-    setFixedHeader();
-  });
+	navItems.forEach(item => {
+		item.addEventListener('click', closeMenu);
+	});
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 768) {
+			closeMenu();
+		}
+	});
 }
 
 App();
